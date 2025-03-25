bf0bc108af1be9e3ea0b91d98ecd88e8c7d71391
unknown <kentdayola@gmail.com>
2025-03-25 17:35:36 +0800

    Added Role

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,12 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
     }
+    protected function authenticated(Request $request, $user)
+    {
+        if ($user->role === 'admin') {
+            return redirect('/admin/dashboard'); // Redirect admin to admin dashboard
+        } else {
+            return redirect('/user/dashboard'); // Redirect regular users to user dashboard
+        }
+    }
 }
