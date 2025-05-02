73a6f84e5e6249bfda3c7c782ed2c87f8c63da62
mark angelo <markpetpet0827@gmail.com>
2025-05-02 16:21:19 +0800

    seeder,controller,routes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Auth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-
+use App\Models\Role;
+use App\Http\Middleware\ifAdmin;
+use App\Http\Middleware\ifUser;
 class LoginController extends Controller
 {
     /*
--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,17 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Admin\HomeController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes();
 
 // Admin Routes
 Route::prefix('admin')->middleware('admin')->group(function () {
-    Route::get('/', [HomeController::class, 'index'])->name('admin.home');
+    Route::get('/admin', [HomeController::class, 'index'])->name('admin.home');
 });
 
 //user routes
-// Route::prefix('user')->name('user')->group(function () {
-//     Route::get('/home', [UserController::class, 'index'])->name('home');
-// });
+ Route::prefix('user')->name('user')->group(function () {
+    Route::get('/home', [UserController::class, 'index'])->name('home');
+ });
